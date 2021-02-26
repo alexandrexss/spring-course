a2261064678733244556ab76c1b8d30d525e43b1
Alexandre Ramos <alexandre.ramos@yahoo.com>
2021-02-26 16:30:56 -0300

    Config serialization

--- src/main/java/com/springcourse/domain/Request.java
+++ src/main/java/com/springcourse/domain/Request.java
@@ -1,6 +1,7 @@
 package com.springcourse.domain;
 
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -31,8 +32,15 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter @Setter
 @Entity(name = "request")
-public class Request {
+public class Request implements Serializable {
 	
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
